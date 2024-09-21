48b65601e912a6081e6bc78dd7e9646d4747b9c5
Bhanu Prakash M <sravan8147@gmail.com>
2024-09-21 21:27:20 +0530

    Update easy_dsa.js

--- easy_dsa.js
+++ easy_dsa.js
@@ -22,3 +22,39 @@ const quickSort = (arr) => {
 
 const test = [2,4,5,9,1,3];
 console.log(quickSort(test));
+
+const mergeSort  = (arr) => {
+    console.log(arr);
+    if(arr.length < 2) return arr;
+    
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0,mid));
+    const right = mergeSort(arr.slice(mid));
+    
+    const sorted =  merge(left, right);
+    
+    return sorted;
+}
+
+
+const merge = (left, right) => { 
+    let leftIndex = 0, rightIndex = 0, temp = [];
+    
+    while(leftIndex < left?.length && rightIndex < right?.length){
+        if(left[leftIndex] <= right[rightIndex]){
+            temp.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            temp.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+    
+    while(leftIndex < left?.length){ temp.push(left[leftIndex]); leftIndex++; }
+    while(rightIndex < right?.length){ temp.push(right[rightIndex]); rightIndex++; }
+    
+    console.log("Merge: ",{left, right, result: temp});
+    return temp;
+}
+
+console.log(mergeSort([ 9, 4, 6,1, 7, 3, 5, 1, 10, 8]))
